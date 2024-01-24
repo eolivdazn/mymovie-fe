17ceb92a517d548dc775757299d77c748b6d7dfb
eolivdazn <eterra1@gmail.com>
2024-01-24 21:55:54 +0000

    icon improve

--- src/components/googleSign/signIn.jsx
+++ src/components/googleSign/signIn.jsx
@@ -3,6 +3,12 @@ import React, { useEffect, useState } from "react";
 import {auth,provider} from "./config";
 import {signInWithPopup} from "firebase/auth";
 import {BsFillInfoCircleFill} from "react-icons/bs";
+import { SlLogin } from "react-icons/sl";
+import { VscAccount } from "react-icons/vsc";
+import { GrLogout } from "react-icons/gr";
+
+
+
 import MenuItem from "@/components/MenuItem";
 // import Home from "./Home";
 
@@ -11,19 +17,39 @@ function SignIn(){
     const handleClick =()=>{
         signInWithPopup(auth,provider).then((data)=>{
             setValue(data.user.email)
+            console.log(JSON.stringify(data.user.uid))
             localStorage.setItem("email",data.user.email)
+            localStorage.setItem("userUID",data.user.uid)//tjv6Ad6ORPfkCkdIHxO7aVW1OVu2
         })
+    }
+    const logoutClick =()=>{
+            localStorage.removeItem("email")
+        setValue('')
     }
 
     useEffect(()=>{
-        setValue(localStorage.getItem('email'))
+        setValue(localStorage.getItem('email')
+        )
     })
     const listUrl = `/movieList?email=${value}`
     return (
 
-        <div>
-            {value? <MenuItem title="Your list" address={listUrl} Icon={BsFillInfoCircleFill} />:
-                <button onClick={handleClick}>Signin With Google</button>
+        <div className="flex">
+            {value ?
+                <>
+                    <MenuItem title="Your list" address={listUrl} Icon={VscAccount}/>
+                    <button onClick={logoutClick}>
+                            <GrLogout className="text-2xl  mx-1"/>
+                    </button>
+                </>
+                :
+                <>
+                    <button onClick={handleClick}>
+                        <p className=" flex sm:inline my-2 text-sm">
+                            <SlLogin className="text-2xl sm:hidden mx-1"/>
+                            <span style={{"padding-top": "2px" }}>In</span></p>
+                    </button>
+                </>
             }
         </div>
     );
